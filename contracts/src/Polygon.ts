a51fbad0f06bab55fbbc52f2d7fd04f1853cdc4b
Illya Gerasymchuk <illya@iluxonchik.me>
2023-10-13 20:03:42 +0100

    feat: correct logic AND, and add technical notes on additional ZKProgram

--- contracts/src/Polygon.ts
+++ contracts/src/Polygon.ts
@@ -46,14 +46,14 @@ export class ThreePointPolygon extends Struct({
 }
 
 class CoordinateProofState extends Struct({
-  insidePolygonCommitment: Field,
+  polygonCommitment: Field,
   // TODO: consider including outSidePolygonCommitment proofs, in order to inlcude the "inner" and "outer" polygon definitions of GeoJSON
   //outsidePolygonCommitment: Field,
   coordinatesCommitment: Field, // IMPORTANT: without a nonce, this leaks the coordinates
   isInPolygon: Bool,
 }) {
   toString(): string {
-    return `Polygon Commitment: ${this.insidePolygonCommitment.toString()}\nCoordinates Commitment: ${this.coordinatesCommitment.toString()}\nIs In Polygon: ${this.isInPolygon.toString()}`;
+    return `Polygon Commitment: ${this.polygonCommitment.toString()}\nCoordinates Commitment: ${this.coordinatesCommitment.toString()}\nIs In Polygon: ${this.isInPolygon.toString()}`;
   }
 }
 
@@ -127,15 +127,15 @@ function proveCoordinatesIn3PointPolygon(
   Provable.log('Is in Polygon ', isInPolygon);
 
   // If point in polygon, return the commitment data
-  const insidePolygonCommitment = polygon.hash();
+  const polygonCommitment = polygon.hash();
   const coordinatesCommitment = point.hash();
 
-  Provable.log('Polygon Commitment: ', insidePolygonCommitment);
+  Provable.log('Polygon Commitment: ', polygonCommitment);
   Provable.log('Coordinates Commitment: ', coordinatesCommitment);
   Provable.log('Is In Polygon: ', isInPolygon);
 
   return new CoordinateProofState({
-    insidePolygonCommitment: insidePolygonCommitment,
+    polygonCommitment: polygonCommitment,
     coordinatesCommitment: coordinatesCommitment,
     isInPolygon: isInPolygon,
   });
@@ -145,7 +145,7 @@ function proveCoordinatesIn3PointPolygon(
  * Given two proofs, it combines them into a single proof that is the AND of the two proofs.
  * The AND operand is applied to the `isInPolygon` field of the two proofs. The proof is computed
  * even if neither of the proofs have `isInPolygon` set to true. The proof verifies that the
- * `coordinatesCommitment` are the same, and that the `insidePolygonCommitment` are different.
+ * `coordinatesCommitment` are the same, and that the `polygonCommitment` are different.
  * @param proof1 - the first proof
  * @param proof2  - the second proof
  * @returns CoordinateProofState
@@ -154,6 +154,33 @@ function AND(
   proof1: SelfProof<Empty, CoordinateProofState>,
   proof2: SelfProof<Empty, CoordinateProofState>
 ): CoordinateProofState {
+
+  // IMPORTANT: this has an issue. If I give proof1, which asserts that the user is in Spain, and proof2 that
+  // asserts that the user is not in Romania, then the resulting proof from .AND will say that the user is
+  // neither in Spain, nor Romania. This is because the AND operation is applied to the `isInPolygon` field
+  // of the two proofs. This is not the desired behaviour. The desired behaviour is to have a proof that
+  // asserts that the user is in Spain AND is not in Romania.
+
+  // For correctness, this method/proof should opearate ensuring that the values of `isInPolygon` is the same
+  // in both proofs: either both are true, or both are false. If they are different, then the proof should
+  // fail. This way the proof will attest to either the user being inside a set of polygons, or outside of that set.
+
+  // A good solution for this appears to be to separate proofs. The proofs in CoordinatesInPolygon should only attest to
+  // either the presence or the abcense of the user from a polygon, or a set of polygons. Simple polygons are combined
+  // into more complex shapes using the AND and OR operations. There should be another ZKProgram, that takes as input
+  // a set of proofs from CoordinatesInPolygon ZKProgram, checks their isInPolygon values, and then combines them
+  // into another proof whose public output includes a commitment to the polygons in which the user is in, and
+  // to the polygons in which the user is not. Wether this should be implemented or not should be carefully considered.
+  // It's important to note, that the existing system can be combined to achieve the same result, but it may not be as
+  // convenient as having a single proof that attests to the presence or absence of the user from a set of polygons.
+  // Proving that you are in Romania or Spain, means proving that you are NOT in the rest of the world.
+
+  // The additional ZKProgram described above can be implemented as an additional feature. It can be considered
+  // as a nice to have. The new ZKProgram would accept two proofs which have CoordinateProofState as their public output,
+  // and combine them into a public output which whould add all of the `isInPolygon=True` polygons to `insidePolygonCommitment`,
+  // and all of the `isInPolygon=False` polygons to `outsidePolygonCommitment`. The new ZKProgram would also verify that
+  // the `coordinatesCommitment` of the two proofs are the same, and that the `polygonCommitment` are different. 
+
   proof1.verify();
   proof2.verify();
 
@@ -161,25 +188,29 @@ function AND(
   proof1.publicOutput.coordinatesCommitment.assertEquals(
     proof2.publicOutput.coordinatesCommitment
   );
+ 
   // ensure that the proof is not done for the same polygon
-  proof1.publicOutput.insidePolygonCommitment.assertNotEquals(
-    proof2.publicOutput.insidePolygonCommitment
+  proof1.publicOutput.polygonCommitment.assertNotEquals(
+    proof2.publicOutput.polygonCommitment
   );
 
-  // logic of AND
-  let isInPolygon = Provable.if(
-    proof1.publicOutput.isInPolygon.and(proof2.publicOutput.isInPolygon),
+  // ensure that the proofs are either both for isInPolygon, or both not for isInPolygon
+  let expectedSecondProofIsInPolygon = Provable.if(
+    proof1.publicOutput.isInPolygon,
     Bool(true),
     Bool(false)
   );
 
+  expectedSecondProofIsInPolygon.assertEquals(proof2.publicOutput.isInPolygon);
+
+  
   return new CoordinateProofState({
-    insidePolygonCommitment: Poseidon.hash([
-      proof1.publicOutput.insidePolygonCommitment,
-      proof2.publicOutput.insidePolygonCommitment,
+    polygonCommitment: Poseidon.hash([
+      proof1.publicOutput.polygonCommitment,
+      proof2.publicOutput.polygonCommitment,
     ]),
     coordinatesCommitment: proof1.publicOutput.coordinatesCommitment,
-    isInPolygon: isInPolygon,
+    isInPolygon: expectedSecondProofIsInPolygon,
   });
 }
 
@@ -187,7 +218,7 @@ function AND(
  * Given two proofs, it combines them into a single proof that is the OR of the two proofs.
  * The OR operand is applied to the `isInPolygon` field of the two proofs. The proof is computed
  * even if neither of the proofs have `isInPolygon` set to true. The proof verifies that the
- * `coordinatesCommitment` are the same, and that the `insidePolygonCommitment` are different.
+ * `coordinatesCommitment` are the same, and that the `polygonCommitment` are different.
  * @param proof1 - the first proof
  * @param proof2 - the second proof 
  * @returns CoordinateProofState
@@ -204,8 +235,8 @@ function OR(
     proof2.publicOutput.coordinatesCommitment
   );
   // ensure that the proof is not done for the same polygon
-  proof1.publicOutput.insidePolygonCommitment.assertNotEquals(
-    proof2.publicOutput.insidePolygonCommitment
+  proof1.publicOutput.polygonCommitment.assertNotEquals(
+    proof2.publicOutput.polygonCommitment
   );
 
   // logic of OR
@@ -216,9 +247,9 @@ function OR(
   );
 
   return new CoordinateProofState({
-    insidePolygonCommitment: Poseidon.hash([
-      proof1.publicOutput.insidePolygonCommitment,
-      proof2.publicOutput.insidePolygonCommitment,
+    polygonCommitment: Poseidon.hash([
+      proof1.publicOutput.polygonCommitment,
+      proof2.publicOutput.polygonCommitment,
     ]),
     coordinatesCommitment: proof1.publicOutput.coordinatesCommitment,
     isInPolygon: isInPolygon,
