5af0b52cef15bc03ff935f3304dfb4f39fa7973b
Illya Gerasymchuk <illya@iluxonchik.me>
2023-12-07 07:23:50 +0000

    test: add tests for ZKNumber

---
+++ contracts/src/tests/api/ZKNumber.test.ts
@@ -0,0 +1,36 @@
+import { Int64 } from "o1js";
+import { ZKNumber } from "../../api/api";
+
+describe('ZKNumber Class Tests', () => {
+    it('testZKNumberConstructionWithValueTypeNumber', () => {
+        const value = 123;
+        const zkNumber = new ZKNumber(value);
+        expect(zkNumber.raw).toBe(value);
+        expect(zkNumber.normalized).toBe(Math.round(value));
+    });
+
+    it('testZKNumberConstructionWithValueTypeString', () => {
+        const value = "456";
+        const zkNumber = new ZKNumber(value);
+        expect(zkNumber.raw).toBe(value);
+        expect(zkNumber.normalized).toBe(Math.round(Number(value)));
+    });
+
+    it('testZKNumberToRawValue', () => {
+        const value = 789;
+        const zkNumber = new ZKNumber(value);
+        expect(zkNumber.rawValue()).toBe(value);
+    });
+
+    it('testZKNumberToNormalizedValue', () => {
+        const value = 101112;
+        const zkNumber = new ZKNumber(value);
+        expect(zkNumber.normalizedValue()).toBe(Math.round(value));
+    });
+
+    it('testZKNumberToZKValue', () => {
+        const value = 131415;
+        const zkNumber = new ZKNumber(value) as any; // Type casting to use extended methods
+        expect(zkNumber.toZKValue()).toEqual(Int64.from(Math.round(value)));
+    });
+});
