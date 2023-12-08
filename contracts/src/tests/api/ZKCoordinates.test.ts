b8c1604d4c0d2dce4876f0fa4db7b4ef62c57159
Illya Gerasymchuk <illya@iluxonchik.me>
2023-12-08 21:39:44 +0000

    test: add tests for geography structures

---
+++ contracts/src/tests/api/ZKCoordinates.test.ts
@@ -0,0 +1,36 @@
+import { ZKCoordinate, ZKLatitude, ZKLongitude } from "../../api/api";
+
+describe('ZKCoordinate, ZKLatitude, and ZKLongitude Tests', () => {
+    it('testZKCoordinateWithValidNumber', () => {
+        const value = 150;
+        const zkCoordinate = new ZKCoordinate(value);
+        expect(zkCoordinate.raw).toBe(value);
+    });
+
+    it('testZKCoordinateWithInvalidNumber', () => {
+        const value = 200; // Invalid longitude value
+        expect(() => new ZKCoordinate(value)).toThrow();
+    });
+
+    it('testZKLatitudeWithValidNumber', () => {
+        const value = 45;
+        const zkLatitude = new ZKLatitude(value);
+        expect(zkLatitude.raw).toBe(value);
+    });
+
+    it('testZKLatitudeWithInvalidNumber', () => {
+        const value = 100; // Invalid latitude value
+        expect(() => new ZKLatitude(value)).toThrow();
+    });
+
+    it('testZKLongitudeWithValidNumber', () => {
+        const value = -120;
+        const zkLongitude = new ZKLongitude(value);
+        expect(zkLongitude.raw).toBe(value);
+    });
+
+    it('testZKLongitudeWithInvalidNumber', () => {
+        const value = -190; // Invalid longitude value
+        expect(() => new ZKLongitude(value)).toThrow();
+    });
+});
