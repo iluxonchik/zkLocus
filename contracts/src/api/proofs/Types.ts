521e63202c9b99cc287ed4258e8d854674e67a75
Illya Gerasymchuk <illya@iluxonchik.me>
2023-12-14 21:31:16 +0100

    refactor: refactor Types and update dependencies

--- contracts/src/api/proofs/Types.ts
+++ contracts/src/api/proofs/Types.ts
@@ -1,12 +1,5 @@
 import { Bool, JsonProof} from "o1js";
 
-
-type Subclass<Class extends new (...args: any) => any> = (new (...args: any) => InstanceType<Class>) & {
-    [K in keyof Class]: Class[K];
-} & {
-    prototype: InstanceType<Class>;
-};
-
 export interface IVerifiedO1JSProof {
     toJSON(): JsonProof;
 }
@@ -15,7 +8,6 @@ export interface IO1JSProof extends IVerifiedO1JSProof{
     verify(): void;
     verifyIf(condition: Bool): void;
 }
-
 
 
 export interface IO1JSProofConstructor {
