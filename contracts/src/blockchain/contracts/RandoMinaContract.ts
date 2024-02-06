d110aabcbad36fc59a120ea27819899ec52cdf92
Illya Gerasymchuk <illya@iluxonchik.me>
2024-02-06 11:15:32 +0000

    feat: uniquely seed PRNG with sender, make nonce private

--- contracts/src/blockchain/contracts/RandoMinaContract.ts
+++ contracts/src/blockchain/contracts/RandoMinaContract.ts
@@ -1,8 +1,27 @@
 import { Field, Poseidon, SmartContract, Struct, ZkProgram, method } from "o1js";
 
-export class PRNGParameters extends Struct({
+
+/**
+ * Represents the public input parameters for a Pseudo-Random Number Generator (PRNG).
+ * 
+ * The PRNG algorithm is defined by the following parameters:
+ * - networkState: The state of the network at the time of the PRNG computation. This value is used to ensure freshness of
+ *     the random number. This provides the PRNG with the security guarantee against brute-force attacks, which could give
+ *     an attacker an advantage in some contexts. This value could be either a single Mina blockchain network attribute, or
+ *     a combination of them. It's important that this attribute is constant, unique and unpredictable for each Mina block.
+ *     The proofs generated with this number will only be valid within the Mina block where this proof was generated.
+ * - sender: The public key of the sender of the PRNG computation. This value is obtained by Poseidon-hasing the public key of the
+ *    sender. This is the second part of the seed, and it is used as a global nonce, to ensure that the random numbers generated
+ *    throughout the network have a unique seed component.
+ * 
+ * Additionally, the following private input is used:
+ * - nonce: Any number, which is used as the "local seed/nonce" per (networkState, sender) pair. This enables each Mina address to
+ *   be able to generate multiple (infinite) number of pseudo-random numbers in a single Mina block. If necesasry, nonce
+ *   can also be used as a public input. This can be either done by extending the methods of the existing smart contract, or defining a new one.
+ */
+export class PublicPRNGParameters extends Struct({
     networkState: Field,
-    nonce: Field,
+    sender: Field, // hash(publicKey)
 }) {
 }
 
@@ -12,14 +31,20 @@ export class PRNGParameters extends Struct({
  */
 export const RandomNumberObservationCircuit = ZkProgram({
     name: "RandomNumberGenerationObservationCircuit",
-    publicInput: PRNGParameters,
+    publicInput: PublicPRNGParameters,
     publicOutput: Field,
 
     methods: {
         generateRandomNumber: {
-            privateInputs: [],
-            method: (parameters: PRNGParameters): Field => {
-                return Poseidon.hash([parameters.networkState, parameters.nonce]);
+            privateInputs: [Field],
+            method: (parameters: PublicPRNGParameters, nonce: Field): Field => {
+                return Poseidon.hash(
+                    [
+                        parameters.networkState, 
+                        parameters.sender, 
+                        nonce, // local seed/nonce
+                    ]
+                );
             }
         },
  
@@ -42,6 +67,9 @@ export class RandomNumberObservationCircuitProof extends ZkProgram.Proof(RandomN
 export class RandoMinaContract extends SmartContract {
     
     @method verifyRandomNumber(observationProof: RandomNumberObservationCircuitProof): void {
+        const claimedSender: Field = observationProof.publicInput.sender;
+        claimedSender.assertEquals(Poseidon.hash(this.sender.toFields()));
+
         const claimedNetworkState: Field = observationProof.publicInput.networkState;
         this.network.stakingEpochData.ledger.hash.requireEquals(claimedNetworkState);
     }
