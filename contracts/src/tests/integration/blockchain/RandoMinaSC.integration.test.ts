0e6a54d8935585d3cfe0282178e031d70e03b3e9
Illya Gerasymchuk <illya@iluxonchik.me>
2024-02-07 20:30:50 +0000

    test: expand and update tests on success case of PRNG

--- contracts/src/tests/integration/blockchain/RandoMinaSC.integration.test.ts
+++ contracts/src/tests/integration/blockchain/RandoMinaSC.integration.test.ts
@@ -3,7 +3,6 @@ import { AccountUpdate, Field, Mina, Poseidon, PrivateKey, PublicKey } from "o1j
 import { RandoMinaContract, RandomNumberObservationCircuit, RandomNumberObservationCircuitProof } from "../../../blockchain/contracts/RandoMinaContract";
 import { NetworkValue } from "o1js/dist/node/lib/precondition";
 
-const isProofsEnabled: boolean = true;
 
 describe('RandoMina Random Number Generator', () => {
   const Local = Mina.LocalBlockchain();
@@ -12,46 +11,45 @@ describe('RandoMina Random Number Generator', () => {
   const feePayer: PrivateKey = Local.testAccounts[0].privateKey;
   //const feePayer: PrivateKey = PrivateKey.fromBase58(PRIVATE_KEY)
   const feePayerPublicKey: PublicKey = feePayer.toPublicKey();
-  const transactionFee = 100_000_000;
+  const transactionFee: number = 100_000_000;
+
+  const feePayerPublicKeyDigest: Field = Poseidon.hash(feePayerPublicKey.toFields());
 
   beforeAll(async () => {
-    if (isProofsEnabled) {
-      console.log("Compiling circuits...");
-      const startTime = Date.now();
-      await RandomNumberObservationCircuit.compile();
-      const endTime = Date.now();
-      console.log("Compilation complete!");
-      console.log(`Proofs compilation took ${endTime - startTime} milliseconds.`);
+    console.log("Compiling circuits...");
+    const startTime = Date.now();
+    await RandomNumberObservationCircuit.compile();
+    const endTime = Date.now();
+    console.log("Compilation complete!");
+    console.log(`Proofs compilation took ${endTime - startTime} milliseconds.`);
 
-      Mina.setActiveInstance(Local);
-      //Mina.setActiveInstance(Berkeley);
+    Mina.setActiveInstance(Local);
+    //Mina.setActiveInstance(Berkeley);
 
-      console.log("Compiling smart contract...");
-      const startTimeSC = Date.now();
-      await RandoMinaContract.compile();
-      const endTimeSC = Date.now();
-      console.log("Compilation complete!");
-      console.log(`Smart contract compilation took ${endTimeSC - startTimeSC} milliseconds.`);
+    console.log("Compiling smart contract...");
+    const startTimeSC = Date.now();
+    await RandoMinaContract.compile();
+    const endTimeSC = Date.now();
+    console.log("Compilation complete!");
+    console.log(`Smart contract compilation took ${endTimeSC - startTimeSC} milliseconds.`);
 
+    const zkAppPrivateKey: PrivateKey = PrivateKey.random();
+    const zkAppAddress: PublicKey = zkAppPrivateKey.toPublicKey();
+    zkAppInstance = new RandoMinaContract(zkAppAddress);
 
-      const zkAppPrivateKey: PrivateKey = PrivateKey.random();
-      const zkAppAddress: PublicKey = zkAppPrivateKey.toPublicKey();
-      zkAppInstance = new RandoMinaContract(zkAppAddress);
+    console.log("Deploying smart contract...");
+    const txn = await Mina.transaction({ sender: feePayerPublicKey, fee: transactionFee }, () => {
+      AccountUpdate.fundNewAccount(feePayerPublicKey);
+      zkAppInstance.deploy();
+    });
+    await txn.prove();
+    txn.sign([feePayer, zkAppPrivateKey]);
+    await txn.send();
+    console.log("Smart contract deployed!");
 
-      console.log("Deploying smart contract...");
-      const txn = await Mina.transaction({sender: feePayerPublicKey, fee: transactionFee}, () => {
-        AccountUpdate.fundNewAccount(feePayerPublicKey);
-        zkAppInstance.deploy();
-      });
-      await txn.prove();
-      txn.sign([feePayer, zkAppPrivateKey]);
-      await txn.send();
-      console.log("Smart contract deployed!");
-
-      console.log(txn.toGraphqlQuery());
-      console.log();
-      console.log(txn.toJSON());
-    }
+    console.log(txn.toGraphqlQuery());
+    console.log();
+    console.log(txn.toJSON());
   });
 
   describe('Success Case', () => {
@@ -60,24 +58,39 @@ describe('RandoMina Random Number Generator', () => {
       const randomNonce: Field = Field.random();
       const currNetworkState: NetworkValue = Mina.activeInstance.getNetworkState();
       const currentState: Field = currNetworkState.stakingEpochData.ledger.hash;
+      const sender: Field = Poseidon.hash(feePayerPublicKey.toFields());
 
       console.log('Generating observation of computation of PRNG...');
-      const randomNumberGenerationObservation: RandomNumberObservationCircuitProof = await RandomNumberObservationCircuit.generateRandomNumber({networkState: currentState, nonce: randomNonce}); 
+      const randomNumberGenerationObservation: RandomNumberObservationCircuitProof = await RandomNumberObservationCircuit.generateRandomNumber(
+        { networkState: currentState, sender: sender }, // public PRNG params
+        randomNonce, // private PRNG param
+      );
       console.log('Observation of computation of PRNG generated!');
 
+      // Extract generated random number from proof. Verificaiton is skipped, because it
+      // is perfomed in the smart contract call below
+      const obtainedRandomNumber: Field = randomNumberGenerationObservation.publicOutput;
+      // Re-construct PRNG algorithm
+      const expectedRandomNumber: Field = Poseidon.hash([currentState, feePayerPublicKeyDigest, randomNonce]);
+
+      const isPRNGCorrect: boolean = obtainedRandomNumber.equals(expectedRandomNumber).toBoolean()
+      expect(isPRNGCorrect).toBe(true);
+
       console.log("Posting proof to blockchain...");
-      const txn = await Mina.transaction({sender: feePayerPublicKey, fee: transactionFee}, () => {
+      const txn: Mina.Transaction = await Mina.transaction({ sender: feePayerPublicKey, fee: transactionFee }, () => {
         zkAppInstance.verifyRandomNumber(randomNumberGenerationObservation);
       });
-      
+
       console.log("\tProving smart contract invocation...");
       await txn.prove();
       console.log("\tSmart contract invocation proved!");
-      txn.sign([feePayer]);
-      const txnId: Mina.TransactionId = await txn.send();
 
+      txn.sign([feePayer]);
+
+      // Assert that transaction was successful. A successful transaction means a valid random num
+      const txnId: Mina.TransactionId = await txn.send();
       expect(txnId).toBeDefined();
-      expect(txnId.isSuccess).toBe(true);   
+      expect(txnId.isSuccess).toBe(true);
     });
   });
 });
