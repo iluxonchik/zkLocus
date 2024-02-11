88ffc020fe9acdf6b38512908599cebd0d66d00a
Illya Gerasymchuk <illya@iluxonchik.me>
2024-02-11 08:58:12 +0000

    feat: add base  contract

---
+++ contracts/src/blockchain/contracts/tokens/zkl/ZKLContract.ts
@@ -0,0 +1,41 @@
+import {
+    SmartContract,
+    state,
+    State,
+    method,
+    DeployArgs,
+    Permissions,
+    UInt64,
+  } from 'o1js';
+  
+  const tokenSymbol = 'ZKL';
+    
+  /**
+   * Token contract for the $ZKL token - the ecosystem token for zkLocus.
+   * 
+   * The token is being developed iteratively, including it's interface.
+   */
+  export class ZKLContract extends SmartContract {
+    @state(UInt64) totalAmountInCirculation = State<UInt64>();
+  
+    deploy(args: DeployArgs) {
+      super.deploy(args);
+  
+      const permissionToEdit = Permissions.proof();
+  
+      this.account.permissions.set({
+        ...Permissions.default(),
+        editState: permissionToEdit,
+        setTokenSymbol: permissionToEdit,
+        send: permissionToEdit,
+        receive: permissionToEdit,
+      });
+    }
+
+    @method init() {
+        super.init();
+        this.account.tokenSymbol.set(tokenSymbol);
+        this.totalAmountInCirculation.set(UInt64.zero);
+      }
+
+}
