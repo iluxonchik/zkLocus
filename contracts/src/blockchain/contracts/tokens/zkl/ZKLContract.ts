224baeaf1abf88344feca3b7fe801d4b5c8d0c0c
Illya Gerasymchuk <illya@iluxonchik.me>
2024-02-13 19:29:17 +0000

    feat: add mint method implementation

--- contracts/src/blockchain/contracts/tokens/zkl/ZKLContract.ts
+++ contracts/src/blockchain/contracts/tokens/zkl/ZKLContract.ts
@@ -16,9 +16,10 @@ import {
    * Token contract for the $ZKL token - the ecosystem token for zkLocus.
    * 
    * The token is being developed iteratively, including it's interface.
+   * 
    */
   export class ZKLContract extends SmartContract {
-    @state(UInt64) totalAmountInCirculation = State<UInt64>();
+    @state(UInt64) circulatingSupply = State<UInt64>();
   
     deploy(args: DeployArgs) {
       super.deploy(args);
@@ -37,7 +38,32 @@ import {
     @method init() {
         super.init();
         this.account.tokenSymbol.set(tokenSymbol);
-        this.totalAmountInCirculation.set(UInt64.zero);
+        this.circulatingSupply.set(UInt64.zero);
+    }
+
+    @method mint(
+        receiverAddress: PublicKey,
+        amount: UInt64,
+        adminSignature: Signature
+      ) {
+        let totalCirculatingSupply: UInt64 = this.circulatingSupply.get();
+        this.circulatingSupply.requireEquals(totalCirculatingSupply);
+    
+        let newTotalAmountInCirculation: UInt64 = totalCirculatingSupply.add(amount);
+
+        adminSignature
+          .verify(
+            this.address,
+            amount.toFields().concat(receiverAddress.toFields())
+          )
+          .assertTrue();
+    
+        this.token.mint({
+          address: receiverAddress,
+          amount,
+        });
+    
+        this.circulatingSupply.set(newTotalAmountInCirculation);
       }
 
 }
