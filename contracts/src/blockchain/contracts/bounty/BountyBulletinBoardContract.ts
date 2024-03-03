3ed19268c2bc912dd808de02fee1330fe5d54249
Illya Gerasymchuk <illya@iluxonchik.me>
2024-03-03 20:23:23 +0000

    feat: add fund bounty implementation with Merkle Trees for bounty info commitment

--- contracts/src/blockchain/contracts/bounty/BountyBulletinBoardContract.ts
+++ contracts/src/blockchain/contracts/bounty/BountyBulletinBoardContract.ts
@@ -7,12 +7,23 @@ import {
     PublicKey,
     Signature,
     MerkleWitness,
+    state,
+    Field,
+    State,
+    MerkleMapWitness,
+    Poseidon,
   } from 'o1js';
  
   /*
   * Merkle Tree witness with a depth of 5. This number is beint iterated upon.
   */
   class MerkleWitness5 extends MerkleWitness(5) {}
+
+  /*
+    "Witness for the key" - a witness for the key/value pair in the Merkle Map. The witness contains all of the
+    necessary intermediate node values in the tree up to the tree's root, in order to compute a root
+    for the tree for any "value" for the "key".
+  */
     
   /**
    * Bounty Bulletin Board is the smart contract that exposes the "Fund Bounty" and "Claim Bounty" interfaces.
@@ -27,19 +38,48 @@ import {
    * in a set of hierarchical interfaces and implementaions.
    */
   export class BountyBulletinBoardContract extends SmartContract {
+    // Root of the Merkle Map containing the bounty funding information.
+    @state(Field) bountyMapRoot = State<Field>();
+
+    @method initState(intialBountyMapRoot: Field) {
+      this.bountyMapRoot.set(intialBountyMapRoot);
+    }
     
     @method fundBounty(
       funderAddress: PublicKey,
       bountyId: UInt64, // the size of the tree will be fixed, but a self-replication mechanism will be implemented
-      zklAmount: UInt64,
-      witness: MerkleWitness5, // path to (funderAddress, bountyId) in the merkle tree
-      zkAmountBefore: UInt64, // the amount of $ZKL in the funder's account before the funding
-      funderSignature: Signature // checking whether this is required, and what will it contain
+      zklAmountIncrement: UInt64,
+      keyWitness: MerkleMapWitness, // witness to the (funderAddress, bountyId) key in the Merkle Map
+      zklAmountBefore: UInt64, // the amount of $ZKL in the funder's account before the funding
+      //funderSignature: Signature // checking whether this is required, and what will it contain
     ) {
+      // 0. Verify Merkle Map root state
+      const initialBountyMapRoot: Field = this.bountyMapRoot.get();
+      this.bountyMapRoot.requireEquals(initialBountyMapRoot);
+
+      // 1/2. Convert amounts data into Field
+      const claimedZKLAmountBeforeField: Field = Poseidon.hash(zklAmountBefore.toFields());
+
       // 1. Verify that current $ZKL amount in the funder's account is equal to zkAmountBefore
       // 2. Transfer the zklAmount from the sender's account to the contract's account
       // 3. Increment the amount by zklAmount
-      // 4. Update the merkle tree root with zkAmountBefore + zklAmount
+      
+      // Here, we will verify the witness and update the value for the key
+      const actualKey: Field = Poseidon.hash([...funderAddress.toFields(), ...bountyId.toFields()]);
+
+      const [claimedInitialBountyMapRoot, claimedKey] = keyWitness.computeRootAndKey(claimedZKLAmountBeforeField);
+      claimedInitialBountyMapRoot.assertEquals(initialBountyMapRoot, "Claimed root is not the actual root");
+      claimedKey.assertEquals(actualKey, "Claimed key is not the actual key");
+
+      // Compute root after incremeneting the value
+      const newZKLAmount: UInt64 = zklAmountBefore.add(zklAmountIncrement);
+      const newZKLAmountField: Field = Poseidon.hash(newZKLAmount.toFields());
+
+      const [newBountyMapRoot, _] = keyWitness.computeRootAndKey(newZKLAmountField);
+
+
+      // 4. Update the Merkle Map root with zkAmountBefore + zklAmount
+      this.bountyMapRoot.set(newBountyMapRoot);
     }
 
     deploy(args: DeployArgs) {
