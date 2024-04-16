1dc896d174fbfc2350fe2a8c90483828aff3ecd9
Illya Gerasymchuk <illya@iluxonchik.me>
2024-04-16 20:14:57 +0100

    feat: add deployer smart contract

---
+++ contracts/src/blockchain/contracts/experiments/DeployerSC.ts
@@ -0,0 +1,26 @@
+import { Account, AccountUpdate, Bool, DeployArgs, Field, PublicKey, SmartContract, State, VerificationKey, method, state, Permissions, Poseidon, Mina} from "o1js";
+import { DeployeeSC } from "./DeployeeSC";
+
+/**
+ * DeployerSC is a smart contract that deploys another Smart Contract.
+ */
+export class DeployerSC extends SmartContract {
+    @state(PublicKey) bountyMapRoot = State<PublicKey>();
+
+    @method deployDeployee(deployeeAddr: PublicKey, args: DeployArgs) { 
+        const deployeeSC: DeployeeSC = new DeployeeSC(deployeeAddr);
+        deployeeSC.deploy(args);
+        deployeeSC.initState(deployeeAddr);
+        
+        // Retrieve the account update of DeployeeSC
+        const deployeeAccountUpdate: AccountUpdate = deployeeSC.self;
+
+        // TODO: ensure that verification key is set to a specific value
+        // [Code is needed here]
+
+        deployeeAccountUpdate.body.update.appState.at(0)?.isSome.assertEquals(Bool(true));
+
+        // Approve the account update of the DeployeeSC
+        this.approve(deployeeAccountUpdate);
+    }
+}
