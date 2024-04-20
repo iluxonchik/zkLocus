040d2cf58b41ef7d9f0cba02789c9ea7acef196a
Illya Gerasymchuk <illya@iluxonchik.me>
2024-04-20 08:30:03 +0100

    feat: update deployee SC for VK-based approach

--- contracts/src/blockchain/contracts/experiments/DeployeeSC.ts
+++ contracts/src/blockchain/contracts/experiments/DeployeeSC.ts
@@ -1,8 +1,20 @@
 import { DeployArgs, PublicKey, SmartContract, State, state , Permissions, method, Field} from "o1js";
 
 export class DeployeeSC extends SmartContract {
-    @method deployDeployee(deployeeAddr: PublicKey) {
-        
-        const deployeeSC: DeployeeSC = new DeployeeSC();
+    @state(Field) deployer = State<Field>();
+    @state(PublicKey) bountyMapRoot = State<PublicKey>();
+
+    deploy(args: DeployArgs) {
+        super.deploy(args);
+        this.account.permissions.set({
+            ...Permissions.default(),
+            editState: Permissions.proofOrSignature(),
+        });
     }
+
+    // TODO: without @method the complication fails
+    @method setDeployer(deployer: Field) {
+        this.deployer.set(deployer);
+    } 
+
 }
